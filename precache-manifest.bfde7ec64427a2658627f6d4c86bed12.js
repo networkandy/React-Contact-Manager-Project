f57bffbd8a1ec4ef2f54ac6bb997d9cb12a8a67d
Anderson M <red2animation@gmail.com>
2019-06-13 20:26:13 -0400

    Updates

---
+++ precache-manifest.bfde7ec64427a2658627f6d4c86bed12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3d38830bd6e84b3cab02ddfa12483c",
+    "url": "/React-Contact-Manager-Project/index.html"
+  },
+  {
+    "revision": "3598c9f8379185d3e7f1",
+    "url": "/React-Contact-Manager-Project/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cffd85f656016b869ed8",
+    "url": "/React-Contact-Manager-Project/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3598c9f8379185d3e7f1",
+    "url": "/React-Contact-Manager-Project/static/js/2.cc084ffd.chunk.js"
+  },
+  {
+    "revision": "cffd85f656016b869ed8",
+    "url": "/React-Contact-Manager-Project/static/js/main.6ed5b89f.chunk.js"
+  },
+  {
+    "revision": "905dd2111412be7e1f8e",
+    "url": "/React-Contact-Manager-Project/static/js/runtime~main.5247ed6d.js"
+  }
+]);
